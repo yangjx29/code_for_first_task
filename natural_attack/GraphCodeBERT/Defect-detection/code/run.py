245e42750b5a1fd38450c0ce7bac1460914c0d61
yangjx29 <2372256530@qq.com>
2024-11-27 15:53:11 +0800

    add GraphCodeBERT微调之后的结果

--- natural_attack/GraphCodeBERT/Defect-detection/code/run.py
+++ natural_attack/GraphCodeBERT/Defect-detection/code/run.py
@@ -667,7 +667,9 @@ def main():
 
 if __name__ == "__main__":
     """
-    CUDA_VISIBLE_DEVICES=0 python run.py --output_dir=./saved_models --model_type=roberta --config_name=microsoft/graphcodebert-base --tokenizer_name=microsoft/graphcodebert-base --model_name_or_path=microsoft/graphcodebert-base --do_eval --do_test --train_data_file=../preprocess/dataset/train.jsonl --eval_data_file=../preprocess/dataset/valid.jsonl --test_data_file=../preprocess/dataset/function.jsonl --epoch 5 --code_length 512 --data_flow_length 128 --train_batch_size 32 --eval_batch_size 64 --learning_rate 2e-5 --max_grad_norm 1.0 --evaluate_during_training --seed 123456 2>&1 | tee testWithoutFinetuning.log
+    CUDA_VISIBLE_DEVICES=0 python run.py --output_dir=./saved_models --model_type=roberta --config_name=microsoft/graphcodebert-base --tokenizer_name=microsoft/graphcodebert-base --model_name_or_path=microsoft/graphcodebert-base --do_eval --do_test --train_data_file=../preprocess/dataset/train.jsonl --eval_data_file=../preprocess/dataset/valid.jsonl --test_data_file=../preprocess/dataset/function.jsonl --epoch 5 --code_length 256 --data_flow_length 64 --train_batch_size 32 --eval_batch_size 64 --learning_rate 2e-5 --max_grad_norm 1.0 --evaluate_during_training --seed 123456 2>&1 | tee testWithoutFinetuning.log
+
+    CUDA_VISIBLE_DEVICES=0 python run.py --output_dir=./saved_models --model_type=roberta --config_name=microsoft/graphcodebert-base --tokenizer_name=microsoft/graphcodebert-base --model_name_or_path=microsoft/graphcodebert-base --do_eval --do_test --train_data_file=../preprocess/dataset/train.jsonl --eval_data_file=../preprocess/dataset/valid.jsonl --test_data_file=../preprocess/dataset/function.jsonl --epoch 5 --code_length 256 --data_flow_length 64 --train_batch_size 32 --eval_batch_size 64 --learning_rate 2e-5 --max_grad_norm 1.0 --evaluate_during_training --seed 123456 2>&1 | tee testWithFinetuning.log
 
     """
     main()
