09b19852245a4f869f6243c4ede9bb83959f71bf
yangjx29 <2372256530@qq.com>
2024-12-21 23:47:59 +0800

    更新算法相关实现

---
+++ PSOWithcoda/CodeTransformationTest/Mutated.c
@@ -0,0 +1,22 @@
+int main () {
+    int a [6] = {100, 50, 20, 10, 5, 1}, b [6];
+    int n, i, k;
+    scanf ("%d", &n);
+    for (i = 0; i < 6; i++)
+        b[i] = 0;
+    for (k = 0; k < 6; k++) {
+        for (i = 0; n >= a[k]; i++) {
+            b[k] += 1;
+        }
+    }
+    for (i = 0; i < 6; i++) {
+        printf ("%d\n", b[i]);
+    }
+    {
+        if (0) {
+            return 0;
+        }
+    }
+    return 0;
+}
+
