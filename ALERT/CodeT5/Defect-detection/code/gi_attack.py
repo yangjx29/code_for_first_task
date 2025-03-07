6fa42c03f129e4b9d0199054741b288f3d87650b
yangjx29 <2372256530@qq.com>
2025-03-07 14:03:05 +0800

    实现Alert在codet5上的代码，目前只做了asr

---
+++ ALERT/CodeT5/Defect-detection/code/gi_attack.py
@@ -0,0 +1,249 @@
+# coding=utf-8
+# @Time    : 2020/8/13
+# @Author  : Zhou Yang
+# @Email   : zyang@smu.edu.sg
+# @File    : gi_attack.py
+'''For attacking CodeBERT models'''
+import sys
+import os
+
+sys.path.append('../../../')
+sys.path.append('../../../python_parser')
+
+import json
+import logging
+import argparse
+import warnings
+import torch
+import time
+from model import CodeT5
+from run import TextDataset
+from utils import set_seed
+from python_parser.parser_folder import remove_comments_and_docstrings
+from utils import Recorder
+from attacker import Attacker
+from transformers import (RobertaForMaskedLM, RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer)
+from transformers import (T5Config, T5ForConditionalGeneration, RobertaTokenizer, RobertaModel)
+
+from codebleu import calc_codebleu
+import fasttext
+import fasttext.util
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.simplefilter(action='ignore', category=FutureWarning) # Only report warning
+
+MODEL_CLASSES = {
+    'roberta': (RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer),
+    'codet5': (T5Config, T5ForConditionalGeneration, RobertaTokenizer),
+}
+
+logger = logging.getLogger(__name__)
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    ## Required parameters
+    parser.add_argument("--train_data_file", default=None, type=str, required=True,
+                        help="The input training data file (a text file).")
+    parser.add_argument("--output_dir", default=None, type=str, required=True,
+                        help="The output directory where the model predictions and checkpoints will be written.")
+
+    ## Other parameters
+    parser.add_argument("--eval_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
+    parser.add_argument("--test_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
+                    
+    parser.add_argument("--model_type", default="bert", type=str,
+                        help="The model architecture to be fine-tuned.")
+    parser.add_argument("--model_name_or_path", default=None, type=str,
+                        help="The model checkpoint for weights initialization.")
+
+    parser.add_argument("--base_model", default=None, type=str,
+                        help="Base Model")
+    parser.add_argument("--csv_store_path", default=None, type=str,
+                        help="Path to store the CSV file")
+
+    parser.add_argument("--mlm", action='store_true',
+                        help="Train with masked-language modeling loss instead of language modeling.")
+    parser.add_argument("--mlm_probability", type=float, default=0.15,
+                        help="Ratio of tokens to mask for masked language modeling loss")
+
+    parser.add_argument("--config_name", default="", type=str,
+                        help="Optional pretrained config name or path if not the same as model_name_or_path")
+    parser.add_argument("--tokenizer_name", default="", type=str,
+                        help="Optional pretrained tokenizer name or path if not the same as model_name_or_path")
+    parser.add_argument("--block_size", default=-1, type=int,
+                        help="Optional input sequence length after tokenization."
+                             "The training dataset will be truncated in block of this size for training."
+                             "Default to the model max input length for single sentence inputs (take into account special tokens).")
+    parser.add_argument("--do_train", action='store_true',
+                        help="Whether to run training.")
+    parser.add_argument("--use_ga", action='store_true',
+                        help="Whether to GA-Attack.")
+    parser.add_argument("--do_eval", action='store_true',
+                        help="Whether to run eval on the dev set.")
+    parser.add_argument("--do_test", action='store_true',
+                        help="Whether to run eval on the dev set.")    
+    parser.add_argument("--eval_batch_size", default=4, type=int,
+                        help="Batch size per GPU/CPU for evaluation.")
+    parser.add_argument('--seed', type=int, default=42,
+                        help="random seed for initialization")
+    parser.add_argument("--cache_dir", default="", type=str,
+                        help="Optional directory to store the pre-trained models downloaded from s3 (instread of the default one)")
+
+
+
+    args = parser.parse_args()
+
+
+    args.device = torch.device("cuda")
+    # Set seed
+    set_seed(args.seed)
+
+
+    args.start_epoch = 0
+    args.start_step = 0
+
+
+    ## Load Target Model
+    checkpoint_last = os.path.join(args.output_dir, 'checkpoint-last') # 读取model的路径
+    if os.path.exists(checkpoint_last) and os.listdir(checkpoint_last):
+        # 如果路径存在且有内容，则从checkpoint load模型
+        args.model_name_or_path = os.path.join(checkpoint_last, 'pytorch_model.bin')
+        args.config_name = os.path.join(checkpoint_last, 'config.json')
+        idx_file = os.path.join(checkpoint_last, 'idx_file.txt')
+        with open(idx_file, encoding='utf-8') as idxf:
+            args.start_epoch = int(idxf.readlines()[0].strip()) + 1
+
+        step_file = os.path.join(checkpoint_last, 'step_file.txt')
+        if os.path.exists(step_file):
+            with open(step_file, encoding='utf-8') as stepf:
+                args.start_step = int(stepf.readlines()[0].strip())
+        logger.info("reload model from {}, resume from {} epoch".format(checkpoint_last, args.start_epoch))
+
+
+    config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+    config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path,
+                                          cache_dir=args.cache_dir if args.cache_dir else None)
+    config.num_labels=1 # 只有一个label?
+    tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name,
+                                                do_lower_case=False,
+                                                cache_dir=args.cache_dir if args.cache_dir else None)
+    if args.block_size <= 0:
+        args.block_size = tokenizer.max_len_single_sentence  # Our input block size will be the max possible for the model
+    args.block_size = min(args.block_size, tokenizer.max_len_single_sentence)
+    if args.model_name_or_path:
+        model = model_class.from_pretrained(args.model_name_or_path,
+                                            from_tf=bool('.ckpt' in args.model_name_or_path),
+                                            config=config,
+                                            cache_dir=args.cache_dir if args.cache_dir else None)    
+    else:
+        model = model_class(config)
+
+    model = CodeT5(model,config,tokenizer,args)
+
+
+    checkpoint_prefix = 'checkpoint-best-acc/model.bin'
+    output_dir = os.path.join(args.output_dir, '{}'.format(checkpoint_prefix))  
+    model.load_state_dict(torch.load(output_dir))      
+    model.to(args.device)
+
+
+    ## Load CodeBERT (MLM) model
+    codet5 = T5ForConditionalGeneration.from_pretrained(args.base_model)
+    tokenizer_mlm = RobertaTokenizer.from_pretrained(args.base_model)
+    codet5.to('cuda') 
+
+    ## Load Dataset
+    eval_dataset = TextDataset(tokenizer, args,args.eval_data_file)
+
+    # Load original source codes
+    source_codes = []
+    generated_substitutions = []
+    with open(args.eval_data_file) as f:
+        for line in f:
+            js=json.loads(line.strip())
+            # code = ' '.join(js['func'].split())
+            code = js['func']
+            source_codes.append(code)
+            generated_substitutions.append(js['substitutes'])
+    assert(len(source_codes) == len(eval_dataset) == len(generated_substitutions))
+
+    success_attack = 0
+    total_cnt = 0
+
+    recoder = Recorder(args.csv_store_path)
+    fasttext_model = fasttext.load_model('/data/yjx/code_for_first_task/PSOWithcoda/VulnerabilityPrediction/code/saved_models/fasttext_model.bin')
+    attacker = Attacker(args, model, tokenizer, codet5, tokenizer_mlm, use_bpe=1, threshold_pred_score=0, fasttext_model=fasttext_model)
+    start_time = time.time()
+    query_times = 0
+    derta_C = 0
+    codeblue_total = 0
+    diversity_total = 0
+    for index, example in enumerate(eval_dataset):
+        example_start_time = time.time()
+        code = source_codes[index]
+        substituions = generated_substitutions[index]
+        code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, names_to_importance_score, nb_changed_var, nb_changed_pos, replaced_words, drop_prob,diversity = attacker.greedy_attack(example, code, substituions,index)
+        attack_type = "Greedy"
+        if is_success == -1 and args.use_ga:
+        # 如果不成功，则使用gi_attack
+            code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, names_to_importance_score, nb_changed_var, nb_changed_pos, replaced_words, drop_prob,diversity= attacker.ga_attack(example, code, substituions,index, initial_replace=replaced_words)
+            attack_type = "GA"
+
+        codeblue = calc_codebleu([code], [adv_code], lang="c", weights=(0.25, 0.25, 0.25, 0.25), tokenizer=None)
+        codeblue_total += codeblue['codebleu']
+        derta_C += drop_prob
+        diversity_total += diversity
+        example_end_time = (time.time()-example_start_time)/60
+        
+        print("Example time cost: ", round(example_end_time, 2), "min")
+        print("ALL examples time cost: ", round((time.time()-start_time)/60, 2), "min")
+        score_info = ''
+        if names_to_importance_score is not None:
+            for key in names_to_importance_score.keys():
+                score_info += key + ':' + str(names_to_importance_score[key]) + ','
+
+        replace_info = ''
+        if replaced_words is not None:
+            for key in replaced_words.keys():
+                replace_info += key + ':' + replaced_words[key] + ','
+        print("Query times in this attack: ", model.query - query_times)
+        print("All Query times: ", model.query)
+        recoder.write(index, code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, score_info, nb_changed_var, nb_changed_pos, replace_info, attack_type, model.query - query_times, example_end_time)
+        query_times = model.query
+        
+        if is_success >= -1 :
+            # 如果原来正确
+            total_cnt += 1
+        if is_success == 1:
+            success_attack += 1
+        
+        if total_cnt == 0:
+            continue
+        print("Success rate: ", 1.0 * success_attack / total_cnt)
+        print("Successful items count: ", success_attack)
+        print("Total count: ", total_cnt)
+        print("Index: ", index)
+        print()
+        
+    print("Average Query times: ", round(model.query /  len(eval_dataset),2))
+    print("Average drop max: ", round(derta_C /  success_attack,2))
+    print("Average drop min: ", round(derta_C /  len(eval_dataset),2))
+    print("Average codeblue max: ", round(codeblue_total / success_attack,2))
+    print("Average codeblue min: ", round(codeblue_total / len(eval_dataset),2))
+    print("Average diversity max: ", round(diversity_total / success_attack,2))
+    print("Average diversity min: ", round(diversity_total / len(eval_dataset),2))
+    print ("ALL EXAMPLE time cost = %.2f min" % ((time.time()-start_time)/60))
+        
+if __name__ == '__main__':
+    main()
+    """
+    CUDA_VISIBLE_DEVICES=3 python gi_attack.py --output_dir=./saved_models --model_type=codet5 --tokenizer_name=Salesforce/codet5-base-multi-sum --model_name_or_path=Salesforce/codet5-base-multi-sum --csv_store_path .greedy_20_20.csv --base_model=Salesforce/codet5-base-multi-sum --train_data_file=../preprocess/dataset/train_subs.jsonl --eval_data_file=../preprocess/dataset/test_subs_gan_0_400.jsonl --test_data_file=../preprocess/dataset/test_subs.jsonl --block_size 512 --eval_batch_size 64 --seed 123456 2>&1 | tee greedy_20_20.log
+    
+    CUDA_VISIBLE_DEVICES=1 python gi_attack.py --output_dir=./saved_models --model_type=codet5 --tokenizer_name=Salesforce/codet5-base-multi-sum --model_name_or_path=Salesforce/codet5-base-multi-sum --csv_store_path ./ga_20_20.csv --base_model=Salesforce/codet5-base-multi-sum --use_ga --train_data_file=../preprocess/dataset/train_subs.jsonl --eval_data_file=../preprocess/dataset/test_subs_gan_0_400.jsonl --test_data_file=../preprocess/dataset/test_subs.jsonl --block_size 512 --eval_batch_size 64 --seed 123456 2>&1 | tee ga_20_20.log
+
+    CUDA_VISIBLE_DEVICES=1 python gi_attack.py --output_dir=./saved_models --model_type=codet5 --tokenizer_name=Salesforce/codet5-base-multi-sum --model_name_or_path=Salesforce/codet5-base-multi-sum --csv_store_path ./Alert_codet5_20_20.csv --base_model=Salesforce/codet5-base-multi-sum --use_ga --train_data_file=../preprocess/dataset/train_subs.jsonl --eval_data_file=../preprocess/dataset/test_subs_gan_0_400.jsonl --test_data_file=../preprocess/dataset/test_subs.jsonl --block_size 512 --eval_batch_size 64 --seed 123456 2>&1 | tee Alert_codet5_20_20.log    
+    """
